e7b12a2bac32c76b2d5c54a8a8e5d7bc73ee71b4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-02 19:06:00 +0100

    Update 12-fizzbuzz.cs

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace _12_fizzbuzz
-{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0)
+                    Console.Write("FizzBuzz");
+                else if(i % 5 == 0)
+                    Console.Write("Buzz");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz");
+                else
+                    Console.Write(i);
+                if (i == 100)
+                    Console.WriteLine("");
+                else
+                    Console.Write(" ");
+            };
         }
     }
-}
+
