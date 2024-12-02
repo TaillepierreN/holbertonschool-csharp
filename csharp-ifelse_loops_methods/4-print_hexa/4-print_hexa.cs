03e8e2057ab083070bc1b56c6e6ea08a7251d911
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-02 17:57:09 +0100

    fix writeline to write

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -9,7 +9,7 @@ namespace _4_print_hexa
             int i = 0;
             while (i <= 98)
             {
-                Console.WriteLine($"{i} = 0x{i:x}\n");
+                Console.Write($"{i} = 0x{i:x}\n");
                 i++;
             }
         }
