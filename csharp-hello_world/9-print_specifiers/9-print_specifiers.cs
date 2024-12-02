2933ee556aa9726e70a9c588bbe83f06e54d9209
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-02 16:31:53 +0100

    Added 9-print_specifier

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine(string.Format("Percent: {0:0.0%}", percent));
+        Console.WriteLine(string.Format("Currency: {0:C2}", currency));
+	}
+}
