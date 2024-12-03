3f5f014c6686694730a47e9f195effb7fb94fdc0
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 08:41:53 +0100

    Update 9-print_specifiers.cs

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,6 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine("Percent: {0:0.00}%", percent * 100);
-        Console.WriteLine($"Currency: {currency.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US"))}");
+        Console.WriteLine($"Percent: {percent:P2}\nCurrency: {currency.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US"))}");
 	}
 }
