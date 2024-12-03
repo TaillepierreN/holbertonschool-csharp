d376d4d3d965ec641f56d765edda002129c9877f
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 10:49:19 +0100

    Added 4-print_list

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        
+        List<int> list = new List<int>();
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+        {
+            Console.WriteLine("");
+            return list;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+            if (i == size - 1)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+            Console.Write($"{i} ");
+        };
+        return list;
+    }
+}
+
