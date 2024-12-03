4256ff0d620f097b25210ae44b53c4290bf51536
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 16:15:37 +0100

    Added 11-multiply_by_2

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string,int> dictionaryMultiplied = new Dictionary<string,int>();
+
+        foreach(KeyValuePair<string,int> item in myDict)
+            dictionaryMultiplied.Add(item.Key, item.Value * 2);
+        
+        return dictionaryMultiplied;
+    }
+}
+
