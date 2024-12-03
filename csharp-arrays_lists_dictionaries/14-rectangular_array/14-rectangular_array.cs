76f5d41137583599e80084e992edd58fba4e193b
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 16:16:25 +0100

    Added 14-rectangural_array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sideSize = 5;
+        int[,] square = new int[sideSize,sideSize];
+        square[2,2] = 1;
+
+        for (int i = 0; i < sideSize; i++)
+        {
+            for (int j = 0; j < sideSize; j++)
+            {
+                if (j == sideSize - 1)
+                {
+                    Console.Write(square[i,j]);
+                    break;
+                }
+                Console.Write($"{square[i,j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
