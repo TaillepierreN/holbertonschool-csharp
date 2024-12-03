59739a4d1d301086665595bd5ec9aff7a665f1f1
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 09:33:21 +0100

    Added 0-print_array and readme

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("");
+                break;
+            }
+            array[i] = i;
+            if (i == size - 1)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+            Console.Write($"{i} ");
+        };
+        return array;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,1 @@
+csharp array, lists and dictionnaries
