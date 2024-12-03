a66ff4a80298b064aff375ed9c1088f74c8a4e63
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 09:36:13 +0100

    Update 0-print_array.cs

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,15 +9,15 @@ class Array
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+        if (size == 0)
+        {
+            Console.WriteLine("");
+            return new int[0];
+        }
 
         int[] array = new int[size];
         for (int i = 0; i < size; i++)
         {
-            if (size == 0)
-            {
-                Console.WriteLine("");
-                break;
-            }
             array[i] = i;
             if (i == size - 1)
             {
