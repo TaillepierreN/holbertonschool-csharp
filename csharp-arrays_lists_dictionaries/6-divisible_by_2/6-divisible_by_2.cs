19b1f33ac04b0d8ebe2607f62b653eef3df94375
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-03 11:51:35 +0100

    Added 6-divisible_by_2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+            List<bool> list = new List<bool>();
+
+            if (myList == null || myList.Count == 0)
+                return null;
+            
+            foreach (int nbr in myList)
+            {
+                if (nbr % 2 == 0)
+                    list.Add(true);
+                else
+                    list.Add(false);
+            }
+            return list;
+        }
+    }
