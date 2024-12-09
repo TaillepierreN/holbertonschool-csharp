12821e5838caa99096d5b153f7e5269aaee023ab
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-09 14:00:13 +0100

    4-queue_enqueue_dequeue

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        System.Console.WriteLine(string.Format("Number of items: {0}", aQueue.Count));
+
+        System.Console.WriteLine(string.Format("{0}", aQueue.Count == 0 ? "Queue is empty" : "First item: " + aQueue.Peek()));
+        
+        aQueue.Enqueue(newItem);
+
+        System.Console.WriteLine(string.Format("Queue contains \"{0}\": {1}", search, aQueue.Contains(search) ? "True" : "False"));
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Contains(search))
+                aQueue.Dequeue();
+        }
+
+        return aQueue;
+    }
+}
