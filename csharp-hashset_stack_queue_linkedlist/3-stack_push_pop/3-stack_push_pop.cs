10c6ff65d7a3cf6e00b31e2c7cdc300ca8df4340
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-09 12:19:33 +0100

    3-stack_push_pop

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        System.Console.WriteLine(string.Format("Number of items: {0}", aStack.Count));
+
+        System.Console.WriteLine(string.Format("{0}",aStack.Count == 0 ? "Stack is empty" : "Top item: " + aStack.Peek()));
+
+        System.Console.WriteLine(string.Format("Stack contains \"{0}\": {1}", search, aStack.Contains(search)? "True" : "False"));
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Contains(search))
+                aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
+
