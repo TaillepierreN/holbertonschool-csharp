595db3b94503c59e47fc6d410039b68a2cc8f635
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-09 16:32:01 +0100

    Added 13-linkedlist_delete

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> headerLink = myLList.First;
+        int i = 0;
+
+        while (headerLink != null)
+        {
+            if (i == index)
+            {
+                myLList.Remove(headerLink);
+                break;
+            }
+            i++;
+            headerLink = headerLink.Next;
+        }
+    }
+}
