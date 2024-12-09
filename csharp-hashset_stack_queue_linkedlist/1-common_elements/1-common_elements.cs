83a29ddef1b5d82615368e6bc2bb5c71db565947
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-09 11:30:58 +0100

    added 1-common_elements

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> common = new HashSet<int>(list1);
+        common.IntersectWith(list2);
+        List<int> commonList = new List<int>(common);
+        return commonList;
+    }
+}
+
