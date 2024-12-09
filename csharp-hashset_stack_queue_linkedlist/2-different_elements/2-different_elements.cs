c243736967ceaee191008f3cbb06f42ea47cd8d8
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-09 11:48:01 +0100

    Added 2-different_elements

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> firstList = new HashSet<int>(list1);
+        firstList.SymmetricExceptWith(list2);
+        List<int> commonList = new List<int>(firstList);
+        commonList.Sort();
+        return commonList;
+    }
+}
+
