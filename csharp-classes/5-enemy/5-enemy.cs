b4904072e3dc2eeb7f3ebf5512439091510064b7
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-06 11:17:27 +0100

    Update 5-enemy.cs

--- csharp-classes/5-enemy/5-enemy.cs
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace _5_enemy
+namespace Enemies
 {
-    class Program
+    /// <summary> Represents a zombie.</summary>
+    public class Zombie
     {
-        static void Main(string[] args)
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary> Initializes a new instance of the zombie class.</summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+
+        /// <summary> Initializes a new instance of the zombie class with a given health value.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        /// <summary> Gets or sets the name of the zombie.</summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary> Gets the health of the zombie. </summary>
+        /// <returns>int health</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
         }
     }
 }
