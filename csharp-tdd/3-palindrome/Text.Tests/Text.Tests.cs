d91b8494f61f0e7ff7b0f8c7a9b9c5b285c73d71
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-06 17:23:09 +0100

    3-palindrome

--- csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,18 +1,49 @@
 using NUnit.Framework;
+using Text;
 
-namespace Tests
+namespace Text.Tests
 {
-    public class Tests
+    public class StrTests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void IsPalindrome_InputTrue_ReturnTrue()
         {
+            string input = "Racecar";
+
+            bool result = Str.IsPalindrome(input);
+
+            Assert.AreEqual(true, result);
         }
 
         [Test]
-        public void Test1()
+        public void IsPalindrome_InputFalse_ReturnFalse()
         {
-            Assert.Pass();
+            string input = "Thomas";
+
+            bool result = Str.IsPalindrome(input);
+
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void IsPalindrome_InputEmpty_ReturnTrue()
+        {
+            string input = "";
+
+            bool result = Str.IsPalindrome(input);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void IsPalindrome_InputTrueWithPunctuation_ReturnTrue()
+        {
+            string input = "A man, a plan, a canal: Panama.";
+
+            bool result = Str.IsPalindrome(input);
+
+            Assert.AreEqual(true, result);
         }
     }
 }
