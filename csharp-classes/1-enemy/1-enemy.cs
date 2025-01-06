181f21468dfb6511471e8ffcc901381edf08e113
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-06 10:45:43 +0100

    added 1-enemy

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents a zombie.</summary>
+    public class Zombie
+    {
+        /// <summary> Health of the zombie.</summary>
+        public int health;
+
+        /// <summary> Initializes a new instance of the zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
