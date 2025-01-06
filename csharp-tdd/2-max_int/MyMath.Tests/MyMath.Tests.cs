d0e923d39ef754df6fc8787a5ca49c92cc83e1a3
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-06 16:57:35 +0100

    2-max_int

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,60 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using MyMath;
+using System;
 
-namespace Tests
+namespace MyMath.Tests
 {
-    public class Tests
+    public class OperationTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Max_Positive_ReturnCorrectOutput()
         {
+            List<int> input = new List<int> { 1, 5, 503, 14, 95 };
+
+            int result = Operations.Max(input);
+
+            Assert.AreEqual(503, result);
         }
 
         [Test]
-        public void Test1()
+        public void Max_Negative_ReturnCorrectOutput()
         {
-            Assert.Pass();
+            List<int> input = new List<int> { -1, -5, -503, -14, -95 };
+
+            int result = Operations.Max(input);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Max_Mixed_ReturnCorrectOutput()
+        {
+            List<int> input = new List<int> { -1, 5, -503, 14, -95 };
+
+            int result = Operations.Max(input);
+
+            Assert.AreEqual(14, result);
+        }
+
+        [Test]
+        public void Max_Null_ReturnZero()
+        {
+            List<int> input = null;
+
+            int result = Operations.Max(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Max_Empty_ReturnZero()
+        {
+            List<int> input = new List<int>();
+
+            int result = Operations.Max(input);
+
+            Assert.AreEqual(0, result);
         }
     }
 }
