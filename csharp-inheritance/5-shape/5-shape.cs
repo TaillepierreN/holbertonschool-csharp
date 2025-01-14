62e33f22c35f0ebcd290c65b49045f6e65e506b1
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-14 11:58:46 +0100

    added 5-shape

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Class for shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Calculate the area of a shape.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
