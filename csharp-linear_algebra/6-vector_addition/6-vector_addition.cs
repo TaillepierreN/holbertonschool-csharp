532fcd1b91965bb4449bbab489633bbf3020f522
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 11:13:23 +0100

    Update 6-vector_addition.cs

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,28 +1,26 @@
 ﻿using System;
 
-namespace _6_vector_addition
+/// <summary>
+/// Class for vector math
+/// </summary>
+class VectorMath
 {
     /// <summary>
-    /// Class for vector math
+    /// Add two 2d / 3d vectors
     /// </summary>
-    class VectorMath
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static double[] Add(double[] vector1, double[] vector2)
     {
-        /// <summary>
-        /// Add two 2d / 3d vectors
-        /// </summary>
-        /// <param name="vector1"></param>
-        /// <param name="vector2"></param>
-        /// <returns></returns>
-        public static double[] Add(double[] vector1, double[] vector2)
-        {
-            if(vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3 
-            || vector2.Length < 2 || vector2.Length > 3)
-                return new double[] {-1};
-            
-            double[] result = new double[vector1.Length];
-            for (int i = 0; i < vector1.Length; i++)
-                result[i] = vector1[i] + vector2[i];
-            return result;
-        }
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
+        || vector2.Length < 2 || vector2.Length > 3)
+            return new double[] { -1 };
+
+        double[] result = new double[vector1.Length];
+        for (int i = 0; i < vector1.Length; i++)
+            result[i] = vector1[i] + vector2[i];
+        return result;
     }
 }
+
