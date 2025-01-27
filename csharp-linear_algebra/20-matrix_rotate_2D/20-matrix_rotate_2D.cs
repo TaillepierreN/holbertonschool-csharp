a421e4a7c2cc1d7f461d27586e780e393f6400bc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 16:46:26 +0100

    19 20

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace _20_matrix_rotate_2D
+/// <summary>
+/// class for matrix math
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// rotate a 2d matrix given an angle in radians
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        int matrixRows = matrix.GetLength(0);
+        int matrixCols = matrix.GetLength(1);
+
+        if (matrixRows != matrixCols || matrixRows != 2)
+            return new double[,] {{-1}};
+        
+        double cosTheta = Math.Cos(angle);
+        double sinTheta = Math.Sin(angle);
+        double[,] rotateMatrix = new double[,]{{cosTheta, -sinTheta}, {sinTheta, cosTheta}};
+
+        double[,] result = new double[matrixRows, matrixCols];
+            for (int i = 0; i < matrixRows; i++)
+                for (int y = 0; y < matrixCols; y++)
+                {
+                    result[i, y] = 0;
+                    for (int z = 0; z < 2; z++)
+                        result[i, y] += matrix[i, z] * rotateMatrix[z, y];
+                }
+        return result;
     }
 }
+
