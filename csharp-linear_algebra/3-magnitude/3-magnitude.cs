a0308097cf3d090f6d467b7a75893efb70b8c5c8
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 10:47:26 +0100

    added 3 magnitude

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return -1;
+        double sum = 0;
+        foreach (double vectron in vector)
+            sum += vectron * vectron;
+        
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
+
