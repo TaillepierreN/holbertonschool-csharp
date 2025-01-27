481fa25668d6eccebcc096f3277b641b024250aa
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 15:02:43 +0100

    15 16

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace _16_matrix_scalar_mul
+/// <summary>
+/// class for matrix math
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// Multiply a matrix by a scalar
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        if (matrix.GetLength(0) !=2 && matrix.GetLength(0) != 3)
+            return new double[,] { { -1 } };
+        
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int y = 0; y < matrix.GetLength(1); y++)
+                result[i, y] = matrix[i, y] * scalar;
+        return result;
     }
 }
+
