e80d2f2acc3911954ce833086c0ea86d8e91c7c8
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 11:47:46 +0100

    10 11 12

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace _12_dot_product
+/// <summary>
+/// Class for vector math
+/// </summary>
+class VectorMath
 {
-    class Program
+    /// <summary>
+    /// Calculate the dot product of two vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static double DotProduct(double[] vector1, double[] vector2)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
+        || vector2.Length < 2 || vector2.Length > 3)
+            return new double[] { -1 };
+        
+        double result = 0;
+        for (int i = 0; i < vector1.Length; i++)
+            result += vector1[i] * vector2[i];
+        return result;
     }
 }
+
