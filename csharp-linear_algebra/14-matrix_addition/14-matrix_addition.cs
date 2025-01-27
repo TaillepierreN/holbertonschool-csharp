3812700b1346bd95e079fe6110288b95022a4d8d
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 14:30:37 +0100

    13 14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace _14_matrix_addition
-{
-    class Program
+/// <summary>
+/// class for matrix math
+/// </summary>
+    class MatrixMath
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// add two matrices
+        /// </summary>
+        /// <param name="matrix1"></param>
+        /// <param name="matrix2"></param>
+        /// <returns></returns>
+        public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
-            Console.WriteLine("Hello World!");
+            if (matrix1.Length != matrix2.Length || matrix1.Length < 2 || matrix1.Length > 3)
+                return new double[,] { {-1}};
+            
+            double[,] result = new double[matrix1.Length, matrix1.Length];
+            for (int i = 0; i < matrix1.Length; i++)
+                for (int y = 0; y < matrix1.Length; y++)
+                    result[i, y] = matrix1[i, y] + matrix2[i, y];
+            return result;
         }
     }
-}
+
