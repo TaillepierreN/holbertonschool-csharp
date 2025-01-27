9cb748a2e968fd95d20473396b7a7302ff6c7031
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 14:38:58 +0100

    Update 14-matrix_addition.cs

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -13,12 +13,17 @@
         /// <returns></returns>
         public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
-            if (matrix1.Length != matrix2.Length || matrix1.Length < 2 || matrix1.Length > 3)
+            int matrix1RowLength = matrix1.GetLength(0);
+            int matrix2RowLength = matrix2.GetLength(0);
+            int matrix1ColLength = matrix1.GetLength(1);
+            int matrix2ColLength = matrix2.GetLength(1);
+
+            if (matrix1RowLength != matrix2RowLength  || matrix1ColLength != matrix2ColLength || (matrix1RowLength != 2 && matrix1RowLength != 3))
                 return new double[,] { {-1}};
             
-            double[,] result = new double[matrix1.Length, matrix1.Length];
-            for (int i = 0; i < matrix1.Length; i++)
-                for (int y = 0; y < matrix1.Length; y++)
+            double[,] result = new double[matrix1RowLength, matrix1ColLength];
+            for (int i = 0; i < matrix1RowLength; i++)
+                for (int y = 0; y < matrix1ColLength; y++)
                     result[i, y] = matrix1[i, y] + matrix2[i, y];
             return result;
         }
