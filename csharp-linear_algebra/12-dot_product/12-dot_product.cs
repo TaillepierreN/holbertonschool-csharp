6736cf261c013e8c4332e28853160eaa2714b4b2
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 11:49:23 +0100

    Update 12-dot_product.cs

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -15,7 +15,7 @@ class VectorMath
     {
         if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
         || vector2.Length < 2 || vector2.Length > 3)
-            return new double[] { -1 };
+            return -1;
         
         double result = 0;
         for (int i = 0; i < vector1.Length; i++)
