8f497dd2126aeb2b5cab9cf76472831e9c702563
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 17:11:42 +0100

    Update 20-matrix_rotate_2D.cs

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,42 +1,42 @@
-﻿using System;
+﻿// using System;
 
-/// <summary>
-/// class for matrix math
-/// </summary>
-class MatrixMath
-{
-    /// <summary>
-    /// rotate a 2d matrix given an angle in radians
-    /// </summary>
-    /// <param name="matrix"></param>
-    /// <param name="angle"></param>
-    /// <returns></returns>
-    public static double[,] Rotate2D(double[,] matrix, double angle)
-    {
-        int matrixRows = matrix.GetLength(0);
-        int matrixCols = matrix.GetLength(1);
+// /// <summary>
+// /// class for matrix math
+// /// </summary>
+// class MatrixMath
+// {
+//     /// <summary>
+//     /// rotate a 2d matrix given an angle in radians
+//     /// </summary>
+//     /// <param name="matrix"></param>
+//     /// <param name="angle"></param>
+//     /// <returns></returns>
+//     public static double[,] Rotate2D(double[,] matrix, double angle)
+//     {
+//         int matrixRows = matrix.GetLength(0);
+//         int matrixCols = matrix.GetLength(1);
 
-        if (matrixRows != matrixCols || matrixRows < 2)
-            return new double[,] { { -1 } };
+//         if (matrixRows != matrixCols || matrixRows < 2)
+//             return new double[,] { { -1 } };
 
-        double cosTheta = Math.Cos(angle);
-        double sinTheta = Math.Sin(angle);
+//         double cosTheta = Math.Cos(angle);
+//         double sinTheta = Math.Sin(angle);
 
-        double[,] result = new double[matrixRows, matrixCols];
+//         double[,] result = new double[matrixRows, matrixCols];
 
-        for (int i = 0; i < matrixRows; i++)
-        {
-            for (int j = 0; j < matrixCols; j++)
-            {
-                int newX = (int)Math.Round(cosTheta * (j - matrixCols / 2) - sinTheta * (i - matrixRows / 2) + matrixCols / 2);
-                int newY = (int)Math.Round(sinTheta * (j - matrixCols / 2) + cosTheta * (i - matrixRows / 2) + matrixRows / 2);
+//         for (int i = 0; i < matrixRows; i++)
+//         {
+//             for (int j = 0; j < matrixCols; j++)
+//             {
+//                 int newX = (int)Math.Round(cosTheta * (j - matrixCols / 2) - sinTheta * (i - matrixRows / 2) + matrixCols / 2);
+//                 int newY = (int)Math.Round(sinTheta * (j - matrixCols / 2) + cosTheta * (i - matrixRows / 2) + matrixRows / 2);
 
-                if (newX >= 0 && newX < matrixCols && newY >= 0 && newY < matrixRows)
-                {
-                    result[newY, newX] = matrix[i, j];
-                }
-            }
-        }
-        return result;
-    }
-}
+//                 if (newX >= 0 && newX < matrixCols && newY >= 0 && newY < matrixRows)
+//                 {
+//                     result[newY, newX] = matrix[i, j];
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
