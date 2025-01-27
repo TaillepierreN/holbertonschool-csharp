ecc25225b182657e69e0f16cd2973056d9ed8d95
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-27 15:22:35 +0100

    17 18

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace _18_matrix_matrix_mul
-{
-    class Program
+/// <summary>
+/// class for matrix math
+/// </summary>
+class MatrixMath
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Multiply two matrice
+        /// </summary>
+        /// <param name="matrix1"></param>
+        /// <param name="matrix2"></param>
+        /// <returns></returns>
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
         {
-            Console.WriteLine("Hello World!");
+            if (matrix1.GetLength(1) != matrix2.GetLength(0))
+                return new double[,] { {-1}};
+            
+            double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+                for (int y = 0; y < matrix2.GetLength(1); y++)
+                    for (int z = 0; z < matrix1.GetLength(1); z++)
+                        result[i, y] += matrix1[i, z] * matrix2[z, y];
+            return result;
         }
     }
-}
+
