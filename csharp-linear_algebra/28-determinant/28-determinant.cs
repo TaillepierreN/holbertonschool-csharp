0d4984b153d5049b7d5256b53349d3038f773e23
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-28 10:43:18 +0100

    Update 28-determinant.cs

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -12,15 +12,12 @@ class MatrixMath
     /// <returns></returns>
     public static double Determinant(double[,] matrix)
     {
-        if (matrix == null || (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3))
-            return -1;
 
         int matrixRows = matrix.GetLength(0);
         int matrixCols = matrix.GetLength(1);
 
         if (matrixRows == 2 && matrixCols == 2)
-            return (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
-        
+            return Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
 
         else if (matrixRows == 3 && matrixCols == 3)
         {
@@ -28,7 +25,7 @@ class MatrixMath
             double d = matrix[1, 0], e = matrix[1, 1], f = matrix[1, 2];
             double g = matrix[2, 0], h = matrix[2, 1], i = matrix[2, 2];
 
-            return a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+            return Math.Round((a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g)), 2);
         }
         return -1;
     }
