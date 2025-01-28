159cf914d4302c185976c1db4d6c09f6d4597ea6
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-28 09:37:24 +0100

    Update 24-matrix_shear_2D.cs

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -10,27 +10,18 @@ class MatrixMath
         int matrixRows = matrix.GetLength(0);
         int matrixCols = matrix.GetLength(1);
 
-        if(matrixRows != matrixCols || matrixRows != 2 || (direction != 'x' && direction != 'y'))
-            returnnew double[,] {{-1}};
-        
-        double[,] shearMatrix;
-        if(direction == 'x')
-        {
-            shearMatrix = new double[,] {{1, factor}, {0, 1}}
-        }
-        else
-        {
-            shearMatrix = new double[,] {{1, 0}, {factor, 1}}
-        }
+        if (matrixRows != matrixCols || matrixRows != 2 || (direction != 'x' && direction != 'y'))
+            returnnew double[,] { { -1} };
 
         double[,] result = new double[matrixRows, matrixCols];
         for (int i = 0; i < matrixRows; i++)
         {
             for (int j = 0; j < matrixCols; j++)
             {
-                result[i, j] = 0;
-                for (int k = 0; k < matrixCols; k++)
-                    result[i, j] += shearMatrix[i, k] * matrix[k, j];
+                if (direction == 'x')
+                    result[i, j] = matrix[i, j] + factor * matrix[i, j];
+                else if (direction == 'y')
+                    result[i, j] = matrix[i, j] + factor * matrix[i, j];
             }
         }
         return result;
