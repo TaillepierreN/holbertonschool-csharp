7d772a05b19ab8ad6d2bd5ded777bf0459580b8a
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-28 09:23:34 +0100

    23 24

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace _24_matrix_shear_2D
+/// <summary>
+/// class for matrix math
+/// </summary>
+class MatrixMath
 {
-    class Program
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        static void Main(string[] args)
+        int matrixRows = matrix.GetLength(0);
+        int matrixCols = matrix.GetLength(1);
+
+        if(matrixRows != matrixCols || matrixRows != 2 || (direction != 'x' && direction != 'y'))
+            returnnew double[,] {{-1}};
+        
+        double[,] shearMatrix;
+        if(direction == 'x')
         {
-            Console.WriteLine("Hello World!");
+            shearMatrix = new double[,] {{1, factor}, {0, 1}}
         }
+        else
+        {
+            shearMatrix = new double[,] {{1, 0}, {factor, 1}}
+        }
+
+        double[,] result = new double[matrixRows, matrixCols];
+        for (int i = 0; i < matrixRows; i++)
+        {
+            for (int j = 0; j < matrixCols; j++)
+            {
+                result[i, j] = 0;
+                for (int k = 0; k < matrixCols; k++)
+                    result[i, j] += shearMatrix[i, k] * matrix[k, j];
+            }
+        }
+        return result;
     }
 }
