cfd89833a7ed482ccf4a594d252a8b6c51289511
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-28 11:24:17 +0100

    31 32 33

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace _33_inverse_2D
+/// <summary>
+/// class for matrix math
+/// </summary>
+class MatrixMath
 {
-    class Program
+    public static double[,] Inverse2D(double[,] matrix)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        int matrixRows = matrix.GetLength(0);
+        int matrixCols = matrix.GetLength(1);
+
+        if (matrixRows != 2 || matrixCols != 2)
+            return new double[,] { { -1 } };
+
+        double determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        if (determinant == 0)
+            return new double[,] {{-1}};
+
+        double[,] result = new double[2, 2];
+        result[0, 0] =  matrix[1, 1] / determinant;
+        result[0, 1] = -matrix[0, 1] / determinant;
+        result[1, 0] = -matrix[1, 0] / determinant;
+        result[1, 1] =  matrix[0, 0] / determinant;
+
+        return result;
     }
 }
