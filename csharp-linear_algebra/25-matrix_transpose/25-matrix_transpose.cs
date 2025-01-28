75c284b4dc7126793c08488e0a5fcad785409cf2
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-28 10:28:32 +0100

    Update 25-matrix_transpose.cs

--- csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -19,7 +19,7 @@ class MatrixMath
         double[,] result = new double[matrix.GetLength(1), matrix.GetLength(0)];
 
         for (int i = 0; i < matrix.GetLength(0); i++)
-            for (int j = 0; j < ùatrix.GetLength(1); j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
                 result[j, i] = matrix[i, j];
         return result;
     }
