14f3c9dd8779a6360c0c4a3320c27619b57352c9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-03-26 11:14:17 +0100

    added 1 user interface

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base
+{
+    /// <summary>Name property</summary>
+    public string name { get; set; }
+
+    /// <summary>ToString override</summary>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+interface IInteractive
+{
+	void Interact();
+}
+
+interface IBreakable
+{
+	int Durability { get; set; }
+	void Break();
+}
+
+interface ICollectable
+{
+	bool isCollected { get; set; }
+	void Collect;
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+	///EMpty
+}
