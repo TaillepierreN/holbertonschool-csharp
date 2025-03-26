424da7a8b296a04eba930d0b3efe5cd70af1c3cf
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-03-26 10:58:25 +0100

    Update 0-abstract_thinking.cs

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -5,18 +5,12 @@
 /// </summary>
 abstract class Base
 {
-	/// <summary>
-	/// Name property
-	/// </summary>
-	public string name {get; set;};
+    /// <summary>Name property</summary>
+    public string name { get; set; };
 
-	/// <summary>
-	/// ToString override
-	/// </summary>
-	/// <returns></returns>
-	public override string ToString()
-	{
-		return $"{name} is a {GetType().Name}";
-	}
+    /// <summary>ToString override</summary>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
 }
-
