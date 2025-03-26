1ae6f1420fe5d7cb5e1e2d5ce81b5e4fb9841829
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-03-26 11:21:25 +0100

    Update 1-user_interface.cs

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -5,34 +5,74 @@
 /// </summary>
 abstract class Base
 {
-    /// <summary>Name property</summary>
-    public string name { get; set; }
+	/// <summary>Name property</summary>
+	public string name { get; set; }
 
-    /// <summary>ToString override</summary>
-    public override string ToString()
-    {
-        return $"{name} is a {GetType().Name}";
-    }
+	/// <summary>ToString override</summary>
+	public override string ToString()
+	{
+		return $"{name} is a {GetType().Name}";
+	}
 }
 
+/// <summary>
+/// interface interactive
+/// </summary>
 interface IInteractive
 {
 	void Interact();
 }
 
+/// <summary>
+/// interface breakable
+/// </summary>
 interface IBreakable
 {
 	int Durability { get; set; }
 	void Break();
 }
 
+/// <summary>
+/// interface collectable
+/// </summary>
 interface ICollectable
 {
 	bool isCollected { get; set; }
 	void Collect();
 }
 
+/// <summary>
+/// test object
+/// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-	///EMpty
+	/// <summary>Durability property</summary>
+	public int Durability { get; set; }
+	/// <summary>isCollected property</summary>
+	public bool isCollected { get; set; }
+
+	/// <summary>
+	/// Interact method
+	/// </summary>
+	public void Interact()
+	{
+		///empty
+	}
+
+	/// <summary>
+	/// Break method
+	/// </summary>
+	public void Break()
+	{
+		///empty
+	}
+
+	/// <summary>
+	/// Collect method
+	/// </summary>
+	public void Collect()
+	{
+		///empty
+
+	}
 }
